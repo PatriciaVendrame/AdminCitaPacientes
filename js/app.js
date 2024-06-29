b666911e1722397f6faa851c4fd36218ca9c2215
PatriciaVendrame <patriciaavendrame@gmail.com>
2024-06-29 15:03:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,9 +6,10 @@ const dateInput            = document.querySelector('#entryDate');
 const symptomsInput        = document.querySelector('#symptoms');
 const form                 = document.querySelector('#form-appointment');
 const appointmentContainer = document.querySelector('#appointment');
-
+let editing                = false;
 // Appointment Object
 const appObj = {
+    id: generateId(),
     patient: '',
     owner: '',
     email: '',
@@ -84,9 +85,12 @@ class AdminAppointment {
 
         // Generating appointments
         this.appointments.forEach( appointment => {
+
+            // Container of data 
             const divAppointment = document.createElement('DIV');
             divAppointment.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10', 'rounded-xl');
 
+            // Data
             const patient     = document.createElement('P');
             patient.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case');
             patient.innerHTML = `<span class="font-bold uppercase">Patient: </span>${appointment.patient}`;
@@ -106,13 +110,36 @@ class AdminAppointment {
             const symptoms     = document.createElement('P');
             symptoms.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case');
             symptoms.innerHTML = `<span class="font-bold uppercase">Symptoms: </span>${appointment.symptoms}`;
-            
+
+            // Container of buttons
+            const buttonContainer = document.createElement('DIV');
+            buttonContainer.classList.add('flex', 'justify-between', 'mt-10');
+
+
+            // Edit and Delete buttons
+            const btnEdit = document.createElement('button');
+            btnEdit.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2', 'btn-edit');
+            btnEdit.innerHTML = 'Edit <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>';
+            const cloneAppointment = structuredClone(appointment)
+            btnEdit.onclick = () => completeDataOnFormEditButton(cloneAppointment);
+
+            const btnDelete = document.createElement('button');
+            btnDelete.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnDelete.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+                   
+            // Add buttons on HTML
+            buttonContainer.appendChild(btnEdit);
+            buttonContainer.appendChild(btnDelete);
+
             // Add data appointment 
             divAppointment.appendChild(patient);
             divAppointment.appendChild(owner);
             divAppointment.appendChild(email);
             divAppointment.appendChild(entryDate);
             divAppointment.appendChild(symptoms);
+            divAppointment.appendChild(buttonContainer);
+
+            
 
             // Add appointment on HTML container
             appointmentContainer.appendChild(divAppointment);
@@ -146,7 +173,7 @@ function submitAppointment(e){
         });
     }
 
-    appointment.addAppointment(appObj);
+    appointment.addAppointment({...appObj});//the parameter is a copy of the object
 
     //Reset form 
     form.reset();
@@ -156,6 +183,7 @@ function submitAppointment(e){
 
 function resetAppObj() {
     Object.assign(appObj, {
+        id: generateId(),
         patient: '',
         owner: '',
         email: '',
@@ -164,4 +192,19 @@ function resetAppObj() {
     });
 }
 
+// Generate unique id
+function generateId() {
+    return Math.random().toString(36).substring(2) + Date.now();
+}
 
+function completeDataOnFormEditButton(appointment){
+    Object.assign(appObj, appointment);
+
+    patientInput.value = appointment.patient;
+    ownerInput.value = appointment.owner
+    emailInput.value = appointment.email;
+    dateInput.value = appointment.entryDate;
+    symptomsInput.value = appointment.symptoms
+
+    editing = true;
+}
